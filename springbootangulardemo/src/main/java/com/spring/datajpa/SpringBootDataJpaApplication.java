e6a204b0cc51560fe1f6b129ece37ccbc7e28e0d
Anita Shinde <126587206+anushinde2002@users.noreply.github.com>
2023-07-10 15:49:29 +0530

    Add files via upload

---
+++ springbootangulardemo/src/main/java/com/spring/datajpa/SpringBootDataJpaApplication.java
@@ -0,0 +1,14 @@
+package com.spring.datajpa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		Object SpringApplication;
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+		System.out.println("Tutorial App is Running....");
+	}
+
+}
